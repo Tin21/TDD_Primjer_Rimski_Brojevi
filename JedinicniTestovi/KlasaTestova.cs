b085d47c02474f01d0fdc16d427886531f38ff7d
Tin Franić <tfranic@foi.hr>
2021-09-09 12:58:29 +0200

    Add project files.

---
+++ JedinicniTestovi/KlasaTestova.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrimjerTDD_a_RimskiBrojevi;
+using System;
+
+namespace JedinicniTestovi
+{
+    [TestClass]
+    public class KlasaTestova
+    {
+        [TestMethod]
+        public void Test_Pretvaranja_Broja_1_U_Rimski()
+        {
+            Konverter konverter = new Konverter();
+            string rimski = konverter.Pretvori(1);
+            Assert.AreEqual(rimski, "I");
+        }
+    }
+}
---
+++ PrimjerTDD-a_RimskiBrojevi/Konverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimjerTDD_a_RimskiBrojevi
+{
+    public class Konverter
+    {
+        public string Pretvori(int broj)
+        {
+            if (broj == 1)
+                return "I";
+            return String.Empty;
+        }
+    }
+}
